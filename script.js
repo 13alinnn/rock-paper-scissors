69c91c68ab861036569b87207b951f469afbbac3
13alinnn <iliescualinus@gmail.com>
2024-08-24 15:09:48 +0300

    finished the javascript, the game plays by invoking the function playGame() in the console

--- script.js
+++ script.js
@@ -24,31 +24,46 @@ function playRound(humanChoice, computerChoice) {
     if(humanChoice === 'rock') {
         if(computerChoice === 'paper') {
             console.log('You lose, paper beats rock!');
+            computerScore+=1;
             return;
         }
         else {
-            console.log('You win!')
+            console.log('You win!');
+            humanScore+=1;
             return;
         }
     }
     if(humanChoice === 'paper') {
         if(computerChoice === 'scissors') {
             console.log('You lose, scissors beat paper!');
+            computerScore += 1;
             return;
         }
         else {
             console.log('You win');
+            humanScore += 1;
             return;
         }
     }
     if(humanChoice === 'scissors') {
         if(computerChoice === 'rock') {
             console.log('You lose, rock beats scissors!');
+            computerScore += 1;
             return;
         }
         else {
             console.log('You win!');
+            humanScore += 1;
             return;
         }
     }
+}
+function playGame() {
+    while(humanScore < 5 && computerScore < 5) {
+        playRound();
+    }
+    if(humanScore === 5) alert("You won the game");
+    else alert("You lost!");
+    humanScore = 0;
+    computerScore = 0;
 }
